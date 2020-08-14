807fe4291a99b38bdcdf618fd8f3dc60418f18a0
Aynur Shauerman <aykuli@ya.ru>
2020-08-15 00:14:00 +0700

    dots animation happen

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // todo Настроить преттиер так, чтоб ставил точка запятой после стоки кода
 // todo настроить вебпак для scss
 // todo может есть способ их сгенерировать по распределнию Гаусса
+const CONFIG = {
+  dotsRadius: 5,
+  deltaX: 40,
+  initXOffset: 80,
+  fps: 50,
+}
+const TWO_PI = 2 * Math.PI
+
 const DOTS_PAGE_1 = [
   270,
   240,
@@ -20,10 +28,25 @@ const DOTS_PAGE_1 = [
   270,
 ]
 const DOTS_PAGE_2 = [270, 215, 235, 225, 270, 250, 265, 210, 235]
-const DELTA_PAGE_1 = 40
-const DELTA_PAGE_2 = 80
-let [x0, x1] = [80, 80]
-let y0 = DOTS_PAGE_1[0]
+
+let dots = []
+function setDotsInitState() {
+  dots = []
+  for (let i = 0; i < DOTS_PAGE_1.length; i++) {
+    const x0 = CONFIG.initXOffset + CONFIG.deltaX * i
+    const x1 =
+      i % 2
+        ? CONFIG.initXOffset + (i + 1) * CONFIG.deltaX
+        : CONFIG.initXOffset + i * CONFIG.deltaX
+    const y0 = DOTS_PAGE_1[i]
+    const y1 = i % 2 ? DOTS_PAGE_2[(i + 1) / 2] : DOTS_PAGE_2[i / 2]
+    const dx = (x1 - x0) / CONFIG.fps
+    const dy = (y1 - y0) / CONFIG.fps
+
+    dots.push({ x0, x1, y0, y1, dx, dy })
+  }
+  console.log("dots: ", dots)
+}
 
 const root = document.getElementById("#root")
 const canvas = document.getElementById("canvas")
@@ -33,10 +56,18 @@ const nextBtn = document.querySelector(".btn__next")
 const btn__restart = document.querySelector(".btn__restart")
 const ctx = canvas.getContext ? canvas.getContext("2d") : null
 
+const drawDot = (index, x, y) => {
+  const circle = new Path2D()
+  ctx.fillStyle = index % 2 ? "white" : "blue"
+  circle.arc(x, y, CONFIG.dotsRadius, 0, TWO_PI)
+  ctx.fill(circle)
+  ctx.stroke(circle)
+}
+
 function draw() {
+  ctx.clearRect(40, 40, 700, 340)
+  setDotsInitState()
   paginator.innerText = 1
-  x0 = 80
-  y0 = DOTS_PAGE_2[0]
 
   if (ctx) {
     ctx.clearRect(40, 40, 700, 340)
@@ -56,86 +87,43 @@ function draw() {
     DOTS_PAGE_1.map((y, i) => {
       if (i !== DOTS_PAGE_1.length - 1) {
         ctx.beginPath()
-        ctx.moveTo(x0 + i * DELTA_PAGE_1, y)
-        ctx.lineTo(x0 + (i + 1) * DELTA_PAGE_1, DOTS_PAGE_1[i + 1])
+        ctx.moveTo(CONFIG.initXOffset + i * CONFIG.deltaX, y)
+        ctx.lineTo(
+          CONFIG.initXOffset + (i + 1) * CONFIG.deltaX,
+          DOTS_PAGE_1[i + 1]
+        )
         ctx.stroke()
       }
 
-      const circle = new Path2D()
-      ctx.fillStyle = i % 2 ? "white" : "blue"
-      circle.arc(x0 + i * DELTA_PAGE_1, y, 5, 0, 2 * Math.PI)
-      ctx.fill(circle)
-      ctx.stroke(circle)
-      ctx.font = "bold 48px serif"
-      ctx.fillText(i, x0 + i * DELTA_PAGE_1, y)
+      drawDot(i, 80 + i * CONFIG.deltaX, y)
     })
   }
 }
 
-var [x, y] = [[], []]
-var delta = []
-
-for (let j = 0; j < DOTS_PAGE_1.length; j++) {
-  x.push(80 + j * DELTA_PAGE_1);
-  y.push(DOTS_PAGE_1[j])
-}
-console.log('x: ', x)
-
+let timer
 function animation() {
-  console.log('if x[1]: ', x[1])
-  console.log("animate")
+  clearTimeout(timer)
   paginator.innerText = 2
-  // ctx.globalCompositeOperation = "source-over"
-  ctx.clearRect(40, 40, 700, 340)
 
-  y.map((item, i) => {
-    // ctx.clearRect(40, 40, 700, 340)
+  timer = setTimeout(() => {
+    ctx.clearRect(40, 40, 700, 340)
 
-    // if (i !== DOTS_PAGE_1.length - 1) {
-    //   ctx.beginPath()
-    //   ctx.moveTo(x, y)
-    //   ctx.lineTo(x + DELTA_PAGE_1, DOTS_PAGE_1[i + 1])
-    //   ctx.stroke()
-    // }
+    dots.map(({ x0, x1, y0, y1, dx, dy }, index) => {
+      drawDot(index, x0, y0)
 
-    const circle = new Path2D()
-    ctx.fillStyle = i % 2 ? "white" : "blue"
-    circle.arc(x[i], item, 5, 0, 2 * Math.PI)
-    ctx.fill(circle)
-    ctx.stroke(circle)
-    ctx.font = "bold 48px serif"
-    ctx.fillText(i, x[i], item)
-  })
-
-  if ((x[1] <= DELTA_PAGE_1) || (y[1] >= DOTS_PAGE_2[1])) {
-    x[1]++
-    y[1]--
-    setTimeout("animation()", 10)
-  }
-
-  if ((x[2] >= DELTA_PAGE_1) || (y[2] >= DOTS_PAGE_2[1])) {
-    x[2]--
-    y[2]--
-    setTimeout("animation()", 10)
-  }
-}
-
-let y1 = 250
-function animation0() {
-  paginator.innerText = 2
-  ctx.clearRect(40, 40, 700, 340)
-
-  const circle = new Path2D()
-  ctx.fillStyle = "white"
-  circle.arc(x1, y1, 5, 0, 2 * Math.PI)
-  ctx.fill(circle)
-  ctx.stroke(circle)
-  ctx.font = "bold 48px serif"
-  ctx.fillText(0, x1, y1)
-
-  if (x1 <= 160 && y1 >= 215) {
-    x1 += 1
-    y1++
-    setTimeout("animation0()", 10)
-  }
+      if (
+        (dx > 0 && x0 < x1) ||
+        (dx < 0 && x0 > x1) ||
+        (dy > 0 && y0 < y1) ||
+        (dy < 0 && y0 > y1)
+      ) {
+        dots[index].y0 += dy
+        dots[index].x0 += dx
+        animation(index)
+      } else {
+        dots[index].x0 = x1
+        dots[index].y0 = y1
+      }
+    })
+  }, 10)
 }
